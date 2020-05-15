de345e2751b59959507702da0aad73023fa8a548
Josiah Nosek <Josiah.Nosek69@myhunter.cuny.edu>
2020-05-15 17:38:20 -0400

    commented out some extra debugs for net utils

--- network_base/headers/netutils.c
+++ network_base/headers/netutils.c
@@ -52,7 +52,7 @@ void send_signal(int *conn_fds, int conn_count, u_char signal)
   {
     if (conn_fds[i] == -1)
       continue;
-    fprintf(stderr, "Sending Sig: %c\n", (char)signal);
+    /*fprintf(stderr, "Sending Sig: %c\n", (char)signal);*/
     send_to_socket(conn_fds[i], &signal, sizeof(u_char));
   }
 }
@@ -83,7 +83,7 @@ int encode_user_struct(char *buffer, user_struct *user)
 {
   int n_bytes;
   n_bytes = sprintf(buffer, AUTH_TEMPLATE, AUTH_FLAG, user->username, user->password);
-  DEBUGSS("Auth String Sent", buffer);
+  //DEBUGSS("Auth String Sent", buffer);
   if (n_bytes < 0)
   {
     perror("Failed to Encode User Struct");
@@ -95,14 +95,14 @@ int encode_user_struct(char *buffer, user_struct *user)
 void decode_user_struct(char *buffer, user_struct *user)
 {
   int flag;
-  DEBUGSS("Auth String Recv", buffer);
+  //DEBUGSS("Auth String Recv", buffer);
   if ((sscanf(buffer, AUTH_TEMPLATE, &flag, user->username, user->password) <= 0))
   {
     perror("Failed to decode user struct string");
     exit(1);
   }
-  DEBUGSS("Decoded Username", user->username);
-  DEBUGSS("Decoded Password", user->password);
+  //DEBUGSS("Decoded Username", user->username);
+  //DEBUGSS("Decoded Password", user->password);
   if (flag != AUTH_FLAG)
   {
     fprintf(stderr, "Failed to decode User Struct");
@@ -177,6 +177,7 @@ void decode_chunk_info_struct_from_buffer(u_char *buffer, chunk_info_struct *chu
 void write_split_to_socket_as_stream(int socket, split_struct *split)
 {
   // 1 byte for flag 4 bytes for split_id and 4 bytes for content_length
+
   u_char payload_buffer[MAX_SEG_SIZE];
   int content_bytes_sent, bytes_to_send_next;
 
@@ -192,10 +193,10 @@ void write_split_to_socket_as_stream(int socket, split_struct *split)
   send_to_socket(socket, payload_buffer, MAX_SEG_SIZE);
 
   content_bytes_sent = bytes_to_send_next;
-  DEBUGSN("content_bytes_sent", content_bytes_sent);
+  //DEBUGSN("content_bytes_sent", content_bytes_sent);
   if (split->content_length > MAX_SEG_SIZE - 9)
   {
-    DEBUGS("Sending remaining buffer");
+    //DEBUGS("Sending remaining buffer");
     send_to_socket(socket, split->content + content_bytes_sent, split->content_length - content_bytes_sent);
   }
 }
@@ -211,13 +212,13 @@ void write_split_from_socket_as_stream(int socket, split_struct *split)
 
   if (payload_buffer[0] == INITIAL_WRITE_FLAG)
   {
-    DEBUGS("Initial Write Flag");
+    //DEBUGS("Initial Write Flag");
 
     decode_int_from_uchar(payload_buffer + 1, &split->id);
     decode_int_from_uchar(payload_buffer + 5, &split->content_length);
 
-    DEBUGSN("Split ID", split->id);
-    DEBUGSN("Content Length", split->content_length);
+    //DEBUGSN("Split ID", split->id);
+    //DEBUGSN("Content Length", split->content_length);
 
     if ((split->content = (u_char *)malloc(split->content_length * sizeof(u_char))) == NULL)
     {
@@ -228,13 +229,13 @@ void write_split_from_socket_as_stream(int socket, split_struct *split)
     bytes_to_recv_next = (bytes_to_recv_next < MAX_SEG_SIZE - 9) ? bytes_to_recv_next : MAX_SEG_SIZE - 9;
     bcopy(payload_buffer + 9, split->content, bytes_to_recv_next);
     content_bytes_recv = bytes_to_recv_next;
-    DEBUGSN("content_bytes_recv", content_bytes_recv);
+    //DEBUGSN("content_bytes_recv", content_bytes_recv);
   }
 
   if (split->content_length > content_bytes_recv)
   {
 
-    DEBUGS("Recevieng rest of buffer");
+    //DEBUGS("Recevieng rest of buffer");
 
     recv_from_socket(socket, split->content + content_bytes_recv, split->content_length - content_bytes_recv);
   }
@@ -250,7 +251,7 @@ void encode_split_struct_to_buffer(u_char *buffer, split_struct *split)
   ptr = &buffer[9];
   for (i = 0; i < split->content_length; i++)
     ptr[i] = split->content[i];
-  DEBUGS("Encoding split_struct Done");
+  //DEBUGS("Encoding split_struct Done");
 }
 
 void decode_split_struct_from_buffer(u_char *buffer, split_struct *split)
@@ -272,5 +273,5 @@ void decode_split_struct_from_buffer(u_char *buffer, split_struct *split)
   ptr = &buffer[2];
   for (i = 0; i < split->content_length; i++)
     split->content[i] = ptr[i];
-  DEBUGS("Decoding split_struct Done");
+  //DEBUGS("Decoding split_struct Done");
 }
